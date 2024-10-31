8267564a9c98299aa7f73c611f55b1ea91bcc8df
AriDev <75741608+ArikSquad@users.noreply.github.com>
2024-10-31 21:18:20 +0200

    feat: gui

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # simple-automated-spammer
-The most useless software you find nowadays
+The most useless software you find nowadays - made to learn a bit of Rust
 
 > [!NOTE]\
 > I made this because I was bored and I decided to 
 > port one of my old projects to Rust. If you want to find the original project
-> (you don't), you can find it [here](https://github.com/ArikSquad/Spammer)
+> (you don't), you can find it [here](https://github.com/ArikSquad/Spammer). There's also a GUI now. why not.
 
 
 ## How to use
-`cargo run`
+`cargo run` or `cargo run -- cli` to run the CLI version
+
+*if you want to build it, use `cargo build --release`*
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,102 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use eframe::egui;
 use enigo::Direction::Click;
 use enigo::{Enigo, Key, Keyboard, Settings};
 use std::{io, thread, time::Duration};
+use std::ops::Add;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && args[1] == "cli" {
+        run_cli();
+    } else {
+        run_gui();
+    }
+}
+
+struct SimpleApp {
+    text: String,
+    count: String,
+    status: String,
+    is_running: bool,
+    time_wait: u32,
+}
+
+impl Default for SimpleApp {
+    fn default() -> Self {
+        Self {
+            text: String::new(),
+            count: String::new(),
+            time_wait: 5,
+            status: String::from(""),
+            is_running: false,
+        }
+    }
+}
+
+impl SimpleApp {
+    fn spam_text(&mut self) {
+        if let Ok(times) = self.count.trim().parse::<u32>() {
+            self.status = String::from("Spamming in ".to_string().add(&self.time_wait.to_string()).add(" seconds..."));  // you can probably see im new to rust
+            self.is_running = true;
+
+            let text = self.text.clone();
+
+            thread::spawn(move || {
+                thread::sleep(Duration::from_secs(5));
+                let mut enigo = Enigo::new(&Settings::default()).unwrap();
+
+                for _ in 1..=times {
+                    for c in text.trim().chars() {
+                        enigo.text(&c.to_string()).unwrap();
+                    }
+                    enigo.key(Key::Return, Click).unwrap();
+                    thread::sleep(Duration::from_millis(150));
+                }
+            });
+        }
+    }
+}
+
+impl eframe::App for SimpleApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("simple-automated-spammer");
+
+            ui.horizontal(|ui| {
+                ui.label("Text:");
+                ui.text_edit_singleline(&mut self.text);
+            });
+
+            // We don't want a slider here.
+            ui.horizontal(|ui| {
+                ui.label("Number of times:");
+                ui.text_edit_singleline(&mut self.count);
+            });
+            ui.add(egui::Slider::new(&mut self.time_wait, 1..=30).text("Time to wait"));
+            if ui.button("Start").clicked() && !self.is_running {
+                self.spam_text();
+            }
+
+            ui.label(&self.status);
+        });
+    }
+}
+
+fn run_gui() {
+    let options = eframe::NativeOptions {
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "simple-automated-spammer",
+        options,
+        Box::new(|_cc| Ok(Box::<SimpleApp>::default())),
+    ).unwrap();
+}
+
+fn run_cli() {
     println!("Text to spam? > ");
     let mut text = String::new();
     io::stdin().read_line(&mut text).expect("Failed to read line");
@@ -20,7 +114,6 @@ fn main() {
         for c in text.trim().chars() {
             enigo.text(&c.to_string()).unwrap();
         }
-
         enigo.key(Key::Return, Click).unwrap();
         println!("Sent {} times", i);
         thread::sleep(Duration::from_millis(150));
